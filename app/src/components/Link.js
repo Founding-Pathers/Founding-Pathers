5c1f1a0f41268e2a538389e6bf295932785aae66
kylasim <112044280+kylasim@users.noreply.github.com>
2024-01-27 01:47:18 +0800

    Account creation FE

--- app/src/components/Link.js
+++ app/src/components/Link.js
@@ -2,8 +2,6 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 
-const preventDefault = (event) => event.preventDefault();
-
 export default function Links({color, value, link}) {
   return (
     <Box
@@ -11,7 +9,6 @@ export default function Links({color, value, link}) {
         fontWeight: "bold",
         padding: "5px"
       }}
-      onClick={preventDefault}
     >
       <Link href={link} color={color} underline="none">{value}</Link>
     </Box>
