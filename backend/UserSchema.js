e55f10a84eb40aa36d48cb15da0287a84893b4d4
Jeremy Goh <jeremygmc_@hotmail.com>
2024-02-04 21:56:53 +0800

    start creating user schema

---
+++ backend/UserSchema.js
@@ -0,0 +1,57 @@
+var mongoose = require("mongoose"),
+  Schema = mongoose.Schema,
+  bcrypt = require("bcrypt"),
+  SALT_WORK_FACTOR = 10;
+
+// Schema for users of app
+const UserSchema = new Schema({
+  first_name: {
+    type: String,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+const User = mongoose.model("users", UserSchema);
+
+// hash password before saving
+UserSchema.pre("save", function (next) {
+  var user = this;
+  if (!user.isModified("password")) return next();
+  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+    if (err) return next(err);
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+UserSchema.methods.comparePassword = function (candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+module.exports = mongoose.model("User", UserSchema);
