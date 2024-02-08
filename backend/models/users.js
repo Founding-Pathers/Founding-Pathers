a7d691ebf3b96a710e7b9d2be6c6487f2fc14b74
Law Zer Kye, Keith <keith.law.2021@scis.smu.edu.sg>
2024-02-09 02:35:00 +0800

    Initializing login and authentication middleware with jwtoken

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,9 +1,11 @@
+const { ObjectId } = require('mongodb');
 const dbo = require("../db/conn");
 
 // Require hashing solution
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 
 async function createUser(email, password) {
+    const saltRounds = 10;
     try {
     const db = await dbo.getDb();
     const usersCollection = db.collection('users');
@@ -18,7 +20,7 @@ async function createUser(email, password) {
     }
 
       // Hash the password
-    const hashedPassword = await bcrypt.hash(password, 12);
+    const hashedPassword = await bcrypt.hashSync(password, saltRounds);
 
       // Create a new user document
     const newUser = {
@@ -29,9 +31,39 @@ async function createUser(email, password) {
     await usersCollection.insertOne(newUser);
     console.log(email)
     console.log("User account successfully created");
+    return newUser
     } catch (error) {
         console.log( "Error creating user", error.message )
     }
 }
 
-module.exports = { createUser };
+async function getUserByEmail(email) {
+    try {
+        const db = await dbo.getDb();
+        const usersCollection = db.collection('users');
+        return await usersCollection.findOne({ email: email });
+    } catch (error) {
+        console.error("Error finding user by email:", error.message);
+        throw error;
+    }
+}
+
+async function getUserById(userId) {
+    try {
+        const db = await dbo.getDb();
+        const usersCollection = db.collection('users');
+        
+        // Convert the userId string to an ObjectId
+        const objectId = new ObjectId(userId);
+        
+        // Query the database using the ObjectId
+        const user = await usersCollection.findOne({ _id: objectId });
+        
+        return user;
+    } catch (error) {
+        console.error("Error finding user by ID:", error.message);
+        throw error;
+    }
+}
+
+module.exports = { createUser, getUserByEmail, getUserById };
--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,21 +1,63 @@
 const express = require('express');
-const { createUser } = require('../models/users');
+const { createUser, getUserByEmail } = require('../models/users');
+const { createSecretToken } = require("../util/SecretToken");
+const bcrypt = require("bcryptjs")
 
 const router = express.Router();
 
 // Route for account creation
-router.post('/register', async (req, res) => {
+router.post('/register', async (req, res, next) => {
   try {
     email = req.body.email;
     password = req.body.password;
 
-    await createUser(email, password);
+    const user = await createUser(email, password);
+    
+    var user_id = user._id.toHexString()
+
+    const token = createSecretToken(user_id);
+    res.cookie("token", token, {
+      withCredentials: true,
+      httpOnly: false,
+    });
 
     // Respond with a success message or redirect as needed
-    res.status(201).json({ message: 'User created successfully' });
+    res.status(201).json({ message: 'User created successfully', success: true, user });
+    next();
   } catch (error) {
     console.error('Error in user registration:', error.message);
     res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+router.post('/login', async (req, res, next) => {
+  try {
+
+    const { email, password } = req.body;
+
+    const user = await getUserByEmail(email);
+
+    if(!email || !password ){
+      return res.json({message: 'All fields are required'})
+    }
+
+    if(!user){
+      return res.json({message: 'User does not exist' }) 
+    }
+
+    const auth = await bcrypt.compare(password, user.password)
+    if (!auth) {
+      return res.json({message: 'Incorrect password or email' }) 
+    }
+     const token = createSecretToken(user._id);
+     res.cookie("token", token, {
+       withCredentials: true,
+       httpOnly: false,
+     });
+     res.status(201).json({ message: "User logged in successfully", success: true });
+     next()
+  } catch (error) {
+    console.error(error);
   }
 });
 
