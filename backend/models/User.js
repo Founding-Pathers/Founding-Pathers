e058523876f40ece364a21779b6bd256db0e5362
Law Zer Kye, Keith <keith.law.2021@scis.smu.edu.sg>
2024-02-19 12:56:11 +0800

    edits to post request for /register
    Added firstName, lastName, createdAt and updatedAt to register

--- backend/models/User.js
+++ backend/models/User.js
@@ -47,11 +47,11 @@ const User = mongoose.model("User", UserSchema);
 //   });
 // });
 
-UserSchema.methods.comparePassword = function (candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
-    if (err) return cb(err);
-    cb(null, isMatch);
-  });
-};
+// UserSchema.methods.comparePassword = function (candidatePassword, cb) {
+//   bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+//     if (err) return cb(err);
+//     cb(null, isMatch);
+//   });
+// };
 
 module.exports = User;
--- backend/routes/account.js
+++ backend/routes/account.js
@@ -11,7 +11,7 @@ router.post("/register", async (req, res, next) => {
   try {
     let db_connect = dbo.getDbLogging();
     // Extract user data from the request body
-    const { email, password } = req.body;
+    const { firstName, lastName, email, password } = req.body;
     // Check if the email is already registered
     const existingUser = await db_connect.collection("userAccount").findOne({ email });
     if (existingUser) {
@@ -22,7 +22,7 @@ router.post("/register", async (req, res, next) => {
     const hashPassword = await bcrypt.hash(password, 12);
     console.log(password);
     console.log(hashPassword);
-    const newUser = new User({ email, password: hashPassword });
+    const newUser = new User({ first_name: firstName, last_name: lastName, email, password: hashPassword, createdAt: new Date(), updatedAt: new Date() });
     // Save the user to the database
     await db_connect.collection("userAccount").insertOne(newUser);
 
@@ -39,7 +39,7 @@ router.post("/register", async (req, res, next) => {
     // Respond with a success message or error message
     res
       .status(201)
-      .json({ message: "User created successfully", success: true, user });
+      .json({ message: "User created successfully", success: true, newUser });
     next();
   } catch (error) {
     console.error("Error in user registration:", error.message);
