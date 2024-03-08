7691c1bdfab53720ad8e40db4acc9362809875a9
Jeremy Goh <jeremygmc_@hotmail.com>
2024-03-08 22:59:25 +0800

    Update userpref.js

--- backend/routes/userpref.js
+++ backend/routes/userpref.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const mongoose = require("mongoose");
 
 // router will be added as a middleware
 // and will take control of requests starting with path /userpref.
@@ -7,9 +6,7 @@ const router = express.Router();
 
 // Connect to the database
 const dbo = require("../db/conn");
-
-// Converts the id from string to ObjectId for the _id.
-const ObjectId = require("mongodb").ObjectId;
+const e = require("express");
 
 // Retrieve list of all user preferences (admin)
 router.route("/userpref").get(async function (req, res) {
@@ -27,7 +24,7 @@ router.route("/userpref").get(async function (req, res) {
 // Retrieve user preference by useraccount id
 router.route("/userpref/").get(function (req, res, next) {
   let db_connect = dbo.getDbLogging();
-  let myquery = { _id: ObjectId(req.params.id) };
+  let myquery = { email: req.body.email };
   db_connect
     .collection("userPreferences")
     .findOne(myquery, function (err, result) {
@@ -36,38 +33,66 @@ router.route("/userpref/").get(function (req, res, next) {
     });
 });
 
+router.route("/userpref/test").get(function (req, res, next) {
+  console.log("body: ", req.body.email);
+  res.json(req.body.email);
+});
+
 // Create a new user preference
-router.route("/userpref/add").post(function (req, resp, next) {
+router.route("/userpref/add").post(async function (req, resp, next) {
   let db_connect = dbo.getDbLogging();
-  let myobj = {
-    id: mongoose.Types.ObjectId(req.body.id),
-    wheelchair_friendly: req.body.wheelchair_friendly === "true" ? true : false,
-    f_and_b: req.body.f_and_b === "true" ? true : false,
-    is_sheltered: req.body.is_sheltered === "true" ? true : false,
-    tourist_attraction: req.body.tourist_attraction === "true" ? true : false,
-    bus_stop: req.body.bus_stop === "true" ? true : false,
-    mrt: req.body.mrt === "true" ? true : false,
-    pickup_dropoff: req.body.pickup_dropoff === "true" ? true : false,
-    nature: req.body.nature === "true" ? true : false,
-    created_at: new Date(),
-    updated_at: new Date(),
-  };
-  db_connect
+
+  const {
+    email,
+    wheelchair_friendly,
+    f_and_b,
+    is_sheltered,
+    tourist_attraction,
+    bus_stop,
+    mrt,
+    pickup_dropoff,
+    nature,
+  } = req.body;
+
+  const userPreference = await db_connect
     .collection("userPreferences")
-    .insertOne(myobj, function (err, res) {
-      if (err) {
-        console.error("Error adding user preference:", err);
-        return resp.status(500).json({ error: "Internal Server Error" });
-      }
-      console.log("User preference added successfully");
-      resp.status(200).json({ message: "User preference added successfully" });
-    });
+    .findOne({ email: email });
+
+  if (userPreference) {
+    return resp.status(409).json({ error: "User preference already exists" });
+  } else {
+    let myobj = {
+      email: email,
+      wheelchair_friendly: wheelchair_friendly === "true" ? true : false,
+      f_and_b: f_and_b === "true" ? true : false,
+      is_sheltered: is_sheltered === "true" ? true : false,
+      tourist_attraction: tourist_attraction === "true" ? true : false,
+      bus_stop: bus_stop === "true" ? true : false,
+      mrt: mrt === "true" ? true : false,
+      pickup_dropoff: pickup_dropoff === "true" ? true : false,
+      nature: nature === "true" ? true : false,
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
+    db_connect
+      .collection("userPreferences")
+      .insertOne(myobj, function (err, res) {
+        if (err) {
+          console.error("Error adding user preference:", err);
+          return resp.status(500).json({ error: "Internal Server Error" });
+        }
+        console.log("User preference added successfully");
+        resp
+          .status(200)
+          .json({ message: "User preference added successfully" });
+      });
+  }
 });
 
 // Update a user preference by id
 router.route("/userpref/update").post(function (req, resp, next) {
   let db_connect = dbo.getDbLogging();
-  let myquery = { id: ObjectId(req.body.id) };
+  let myquery = { email: req.body.email };
   let newvalues = {
     $set: {
       wheelchair_friendly:
