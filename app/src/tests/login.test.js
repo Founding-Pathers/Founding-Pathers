77ddcf8440d1eda54225968d05c0cdc120daa331
Law Zer Kye, Keith <keith.law.2021@scis.smu.edu.sg>
2024-03-15 17:33:19 +0800

    Login Page Jest Test and CI Workflow for appJestTest

---
+++ app/src/tests/login.test.js
@@ -0,0 +1,66 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import App from '../App';
+import fetchMock from 'jest-fetch-mock';
+import { act } from 'react-test-renderer';
+
+fetchMock.enableMocks();
+
+const mockNavigate = jest.fn();
+jest.mock('react-router', () => ({
+    ...jest.requireActual('react-router'),
+    useNavigate: () => mockNavigate,
+}));
+
+describe('Application frontend', () => {
+
+test('renders the landing page', () => {
+    render(<App />);
+
+    expect(screen.getByTestId('email-form'))
+    .toBeInTheDocument();
+    expect(screen.queryByText('Email'))
+    .toBeInTheDocument();
+
+    const passwordFormElement = screen.getByTestId('password-form')
+    const passwordInput = passwordFormElement.querySelector('input[type="password"]');
+    expect(passwordInput)
+    .toBeInTheDocument()
+
+    const errorMessage = passwordFormElement.querySelector('.error');
+    expect(errorMessage)
+    .toBeNull();
+});
+
+test('submitting an empty form', async () => {
+    render(<App />)
+
+    const submitButtonElement = screen.getByTestId('ArrowForwardIcon')
+    fireEvent.click(submitButtonElement)
+
+    await screen.findByText('Email is required')
+    await screen.findByText('Password is required')
+});
+
+test('submitting a filled form', async () => {
+
+    fetchMock.mockResponseOnce(JSON.stringify("Success"));
+
+    render(<App />);
+
+    const emailField = screen.getByTestId('email-form').querySelector('input[type="text"]');
+    fireEvent.change(emailField, { target: { value: 'test@example.com' } });
+
+    const passwordField = screen.getByTestId('password-form').querySelector('input[type="password"]');
+    fireEvent.change(passwordField, { target: { value: 'test123' } });
+
+    const formSubmit = screen.getByRole('button');
+    await act(async () => {
+        await fireEvent.click(formSubmit);
+    });
+
+    expect(fetchMock).toHaveBeenCalled();
+
+    expect(mockNavigate).toHaveBeenCalledWith("/home");
+
+    });
+});
--- terraform/providers.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-  region     = "ap-southeast-1a"
-  access_key = "${{}}"
-  secret_key = "your_secret_key"
-}
