72c21bb0869d5072c98485c1e85e2d9c8f29eaac
Jeremy Goh <jeremygmc_@hotmail.com>
2024-03-16 15:39:30 +0800

    Update shortestpath.js
    made it faster using geospatial queries on node dataset

--- backend/routes/shortestpath.js
+++ backend/routes/shortestpath.js
@@ -25,51 +25,57 @@ router.route("/shortestroutes").get(async function (req, res) {
 });
 
 // Retrieve routes according to where the user is at through lat and long
-router.route("/shortestroute").post(function (req, res) {
+router.route("/shortestroute").post(async function (req, res) {
   let db_connect = dbo.getDbRoutes();
-  db_connect
-    .collection("shortest_route")
-    .find()
-    .toArray()
-    .then((data) => {
-      let best_origin_lat = 0;
-      let best_origin_long = 0;
-      let best_dest_lat = 0;
-      let best_dest_long = 0;
-      // for each row, check if the lat and long is the closest to the user's lat and long
-      // save the best lat and long
-      // filter the data to only include routes with the best lat and long
-      // return the data
-      for (let i = 0; i < data.length; i++) {
-        if (
-          Math.abs(req.body.origin_lat - data[i].properties.Origin_LAT) <
-            Math.abs(req.body.origin_lat - best_origin_lat) &&
-          Math.abs(req.body.origin_long - data[i].properties.Origin_LONG) <
-            Math.abs(req.body.origin_long - best_origin_long)
-        ) {
-          best_origin_lat = data[i].properties.Origin_LAT;
-          best_origin_long = data[i].properties.Origin_LONG;
-        }
-        if (
-          Math.abs(req.body.dest_lat - data[i].properties.Dest_LAT) <
-            Math.abs(req.body.dest_lat - best_dest_lat) &&
-          Math.abs(req.body.dest_long - data[i].properties.Dest_LONG) <
-            Math.abs(req.body.dest_long - best_dest_long)
-        ) {
-          best_dest_lat = data[i].properties.Dest_LAT;
-          best_dest_long = data[i].properties.Dest_LONG;
-        }
-      }
-      data = data.filter(
-        (data) =>
-          data.properties.Origin_LAT === best_origin_lat &&
-          data.properties.Origin_LONG === best_origin_long &&
-          data.properties.Dest_LAT === best_dest_lat &&
-          data.properties.Dest_LONG === best_dest_long
-      );
-      // console.log(data);
-      res.json(data);
-    });
+  db_connect.collection("nodes").createIndex({ geometry: "2dsphere" });
+  const origin = await db_connect
+    .collection("nodes")
+    .find({
+      geometry: {
+        $near: {
+          $geometry: {
+            type: "Point",
+            coordinates: [
+              parseFloat(req.body.origin_long),
+              parseFloat(req.body.origin_lat),
+            ],
+          },
+          $maxDistance: 1000,
+        },
+      },
+    })
+    .limit(1)
+    .toArray();
+  let origin_id = origin[0].properties.ORIGIN_ID;
+
+  const dest = await db_connect
+    .collection("nodes")
+    .find({
+      geometry: {
+        $near: {
+          $geometry: {
+            type: "Point",
+            coordinates: [
+              parseFloat(req.body.dest_long),
+              parseFloat(req.body.dest_lat),
+            ],
+          },
+          $maxDistance: 1000,
+        },
+      },
+    })
+    .limit(1)
+    .toArray();
+  let dest_id = dest[0].properties.ORIGIN_ID;
+
+  let route_id = origin_id + ":" + dest_id;
+
+  const route = await db_connect
+    .collection("shortest_path")
+    .find({ "properties.ROUTE_ID": route_id })
+    .toArray();
+
+  res.json({ route });
 });
 
 module.exports = router;
