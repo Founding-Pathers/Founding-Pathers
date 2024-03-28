5c28736e6fd3366c6320e1c7df34242eba8eaa9f
qiaokoli <meganlam10@gmail.com>
2024-03-28 14:23:56 +0800

    changed validation pages to modals

---
+++ app/src/pages/ValidationModals.js
@@ -0,0 +1,66 @@
+{/*
+import Modal from '../components/ui/RouteModal';
+
+const [isDestinationModalOpen, setIsDestinationModalOpen] = useState(false);
+const handleOpenDestinationModal = () => {
+  setIsDestinationModalOpen(true);
+};
+
+const [isFeedbackModalOpen, setIsFeedbackModalOpen] = useState(false);
+const handleOpenFeedbackModal = () => {
+  setIsFeedbackModalOpen(true);
+};
+
+const [isInstructionsModalOpen, setIsInstructionsModalOpen] = useState(false);
+const handleOpenInstructionsModal = () => {
+  setIsInstructionsModalOpen(true);
+};
+const handleCloseInstructionsModal = () => {
+  setIsInstructionsModalOpen(false);
+};
+
+const [isThankYouModalOpen, setIsThankYouModalOpen] = useState(false);
+const handleOpenThankYouModal = () => {
+  setIsThankYouModalOpen(true);
+};
+
+<button onClick={handleOpenDestinationModal}>Destination</button>
+<Modal isOpen={isDestinationModalOpen}
+  title1="You have reached your "
+  title2="destination"
+  title3="!"
+  description2="Please report any issues along your route, such as road closures"
+  buttonText1="Issue to Report"
+  onClick1="/validation"
+  buttonText2="Nothing to Report"
+  onClick2="/feedback"
+/>
+
+<button onClick={handleOpenFeedbackModal}>Feedback</button>
+<Modal isOpen={isFeedbackModalOpen}
+  title1="Thank you for navigating with us!"
+  description2="Do take a short moment to fill up this feedback form to let us learn about your experience with UR-Active!"
+  buttonText1="Feedback Form"
+  onClick1="/feedback"
+/>
+
+<button onClick={handleOpenInstructionsModal}>Instructions</button>
+<Modal isOpen={isInstructionsModalOpen}
+  title1="Report an issue"
+  description1="1. Tap on the point(s) "
+  description2="on the route where you encountered the issue"
+  description3="2. Tap 'NEXT' "
+  description4="to leave a comment on the issue"
+  buttonText1="START"
+  onClick1={handleCloseInstructionsModal}
+/>
+
+<button onClick={handleOpenThankYouModal}>ThankYou</button>
+<Modal isOpen={isThankYouModalOpen}
+  title1="Thank you for your input!"
+  description2="Do take a short moment to fill up this feedback form to let us learn about your experience with UR-Active!"
+  buttonText1="Feedback Form"
+  onClick1="/feedback"
+/>
+
+*/}
